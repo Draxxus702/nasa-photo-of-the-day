4e67ab86c26925cdef0d691756fb85f9c4dfd36f
Draxxus702 <58435416+KennithHowe2@users.noreply.github.com>
2020-01-23 19:37:44 -0800

    added stylings

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Header from './Header'
 function App() {
   return (
     <div className="App">
-     <div>
      <Header/>
-       </div> 
        <Page/>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 
+
 function Header(){
     return(
         <div className='App-header'>
-            <div className='nasa-img'><img className='nasa-imgs' src='/images/nasa-logo.png'></img></div>
-            <div className='nasa-title'><h3><a href='https://www.nasa.gov/' target='_blank'>Nasa</a></h3></div>
+            <div className='nasa-img'><img className='nasa-imgs' src='/images/nasa-logo.png' alt="Nasa"></img></div>
+            <div className='nasa-title'><h1><a href='https://www.nasa.gov/' target='_blank'  rel="noopener noreferrer">NASA</a></h1></div>
         
 
 
